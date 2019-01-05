34939701401f26fcdf54b01f85ca9d4c0989d961
Scott Schottler <scottschottler@gmail.com>
2019-01-05 14:12:25 -0500

    added subscriptions for vote notifications

--- hackernews-node/src/resolvers/Subscription.js
+++ hackernews-node/src/resolvers/Subscription.js
@@ -4,9 +4,23 @@ function newLinkSubscribe(parent, args, context, info) {
 
 const newLink = {
   subscribe: newLinkSubscribe,
-  resolve: payload => payload
+  resolve: payload => {
+    return payload
+  }
+}
+
+function newVoteSubscribe(parent, args, context, info) {
+  return context.prisma.$subscribe.vote({ mutation_in: ['CREATED'] }).node()
+}
+
+const newVote = {
+  subscribe: newVoteSubscribe,
+  resolve: payload => {
+    return payload
+  }
 }
 
 module.exports = {
-  newLink
+  newLink,
+  newVote
 }
