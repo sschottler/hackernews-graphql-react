47be9e4a7454c694a5fb6fcab2bb5d466bc88a9f
Scott Schottler <scottschottler@gmail.com>
2019-01-05 19:58:00 -0500

    JWT Authentication done

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Header from './Header'
+import LinkList from './LinkList'
+import CreateLink from './CreateLink'
+import Login from './Login'
 import '../styles/App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
       </div>
     )
   }
---
+++ frontend/src/components/CreateLink.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const POST_MUTATION = gql`
+  mutation POST_MUTATION($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+
+class CreateLink extends Component {
+  state = {
+    description: '',
+    url: ''
+  }
+
+  render() {
+    const { description, url } = this.state
+
+    return (
+      <div>
+        <div className="flex flex-column mt3">
+          <input
+            type="text"
+            value={description}
+            onChange={e => this.setState({ description: e.target.value })}
+            placeholder="A description for the link"
+            className="mb2"
+          />
+          <input
+            type="text"
+            value={url}
+            onChange={e => this.setState({ url: e.target.value })}
+            placeholder="The URL for the link"
+            className="mb2"
+          />
+          <Mutation
+            mutation={POST_MUTATION}
+            variables={{ description, url }}
+            onCompleted={() => this.props.history.push('/')}>
+            {postMutation => <button onClick={postMutation}>Submit</button>}
+          </Mutation>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CreateLink
